2597d5e35716ec635a7a505cd512064fc6aa5752
Fraser Jubb <fraser.jubb@gmail.com>
2026-01-10 11:14:09 +0000

    Updated logic so that at least one of each selected parameter will be chosen and characters can appear more than once

--- app.js
+++ app.js
@@ -41,18 +41,57 @@ function shuffle(array) {
 //   return activeParams;
 // }
 
+function selectRandom(array) {
+  // console.log(Math.floor(Math.random() * array.length));
+  return Math.floor(Math.random() * array.length);
+}
+
 function getPassword(length) {
+  const preShuffleArr = [];
+
+  console.log(preShuffleArr);
+
   const activeChars = Object.values(passwordParameters)
     .filter(param => param.enabled)
-    .map(param => param.chars)
-    .join('')
-    .split('');
+    .map(param => param.chars.split(''));
+  // .join('')
+  // .split('');
 
-  shuffle(activeChars);
+  // console.log(selectRandom(activeChars[0]));
+  // Get at least one of each selected Parameter
+  for (let i = 0; i < activeChars.length; i++) {
+    const selectedCharacter = activeChars[i][selectRandom(activeChars[i])];
+    // console.log(selectedCharacter);
+    preShuffleArr.push(selectedCharacter);
+  }
 
-  const generatedPassword = activeChars.slice(0, length).join('');
+  const remainingLength = length - activeChars.length;
+  // console.log(selectRandom(activeChars));
+  // console.log(activeChars[selectRandom(activeChars)]);
+  // console.log(activeChars.length);
+  for (let i = 0; i < remainingLength; i++) {
+    // console.log(activeChars[selectRandom(activeChars)]);
+    const chosenArray = activeChars[selectRandom(activeChars)];
+    const randomCharacter = chosenArray[selectRandom(chosenArray)];
+    preShuffleArr.push(randomCharacter);
+    // console.log(i);
+    // console.log(selectRandom(activeChars[i]));
+  }
+  preShuffleArr.join('');
+  console.log(preShuffleArr);
+
+  // const selectRandom = Math.floor(Math.random() * activeChars.length);
+  // console.log(selectRandom);
+  // console.log(activeChars[selectRandom]);
+
+  // shuffle(activeChars);
+
+  // const generatedPassword = activeChars.slice(0, length).join('');
   // console.log(generatedPassword);
-  return generatedPassword;
+
+  shuffle(preShuffleArr);
+  console.log(preShuffleArr);
+  // return generatedPassword;
 }
 
 /*
@@ -61,7 +100,9 @@ EVENT LISTENERS:
 ********************************
 */
 // console.log(getPassword(4));
-getPassword(25);
+getPassword(30);
+// selectRandom([2, 4, 5]);
 // passwordParameters.includeNumbers.enabled = false;
+// console.log('again');
 // passwordParameters.includeSymbols.enabled = false;
 // getPassword(25);
