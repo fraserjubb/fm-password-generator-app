011995281520053589b23c2a0ff738e475bd7be0
Fraser Jubb <fraser.jubb@gmail.com>
2026-01-10 12:34:04 +0000

    Removed all old code that was commented out and no longer needed. Added notes, improved function and variable names so that it is easier to follow

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ DOM SELECTORS:
 
 /* 
 ********************************
-GLOBAL VARIABLES:
-********************************
-*/
-
-/* 
-********************************
-FUNCTIONS:
+GLOBAL VARIABLES / OBJECTS:
 ********************************
 */
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -27,74 +21,60 @@ const characterGroups = {
   includeSymbols: { enabled: true, chars: `!@#$%^&*()-_+={}[]|\\:;"',.?/~` },
 };
 
+/* 
+********************************
+FUNCTIONS:
+********************************
+*/
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const randomChar = Math.floor(Math.random() * (i + 1));
-
     [array[i], array[randomChar]] = [array[randomChar], array[i]];
   }
 }
 
-// function getActiveParams() {
-//   const activeParams = Object.fromEntries(Object.entries(passwordParameters).filter(([_, { enabled }]) => enabled));
-
-//   return activeParams;
-// }
-
-function selectRandom(array) {
+function getRandomIndex(array) {
   // console.log(Math.floor(Math.random() * array.length));
   return Math.floor(Math.random() * array.length);
 }
 
 function getPassword(requestedLength) {
-  const preShuffleArr = [];
+  const passwordDraft = [];
 
-  console.log(requestedLength);
+  // Find character groups that are enabled:
   const enabledCharacterGroups = Object.values(characterGroups)
     .filter(param => param.enabled)
     .map(param => param.chars.split(''));
-  // .join('')
-  // .split('');
-  // console.log(activeChars.length);
+
+  // Prevent a password being generated if the enabled character groups is zero:
+  if (enabledCharacterGroups.length === 0) {
+    throw new Error('No character groups have been selected for character generation.');
+  }
+
+  // Prevent a password being generated if the requested length is too short:
   if (enabledCharacterGroups.length > requestedLength) {
     throw new Error('The requested password length is shorter than the enabled character groups.');
   }
-  // console.log(selectRandom(activeChars[0]));
-  // Get at least one of each selected Parameter
+
+  // Get at least one character from each selected character group:
   for (let i = 0; i < enabledCharacterGroups.length; i++) {
-    const selectedCharacter = enabledCharacterGroups[i][selectRandom(enabledCharacterGroups[i])];
-    // console.log(selectedCharacter);
-    preShuffleArr.push(selectedCharacter);
+    const selectedCharacter = enabledCharacterGroups[i][getRandomIndex(enabledCharacterGroups[i])];
+    passwordDraft.push(selectedCharacter);
   }
-  // console.log(preShuffleArr.length);
-  // console.log(activeChars.length);
+
+  // Get the remaining characters:
   const remainingLength = requestedLength - enabledCharacterGroups.length;
-  // console.log(selectRandom(activeChars));
-  // console.log(activeChars[selectRandom(activeChars)]);
-  // console.log(activeChars.length);
   for (let i = 0; i < remainingLength; i++) {
-    // console.log(activeChars[selectRandom(activeChars)]);
-    const chosenArray = enabledCharacterGroups[selectRandom(enabledCharacterGroups)];
-    const randomCharacter = chosenArray[selectRandom(chosenArray)];
-    preShuffleArr.push(randomCharacter);
-    // console.log(i);
-    // console.log(selectRandom(activeChars[i]));
+    const chosenArray = enabledCharacterGroups[getRandomIndex(enabledCharacterGroups)];
+    const randomCharacter = chosenArray[getRandomIndex(chosenArray)];
+    passwordDraft.push(randomCharacter);
   }
-  preShuffleArr.join('');
-  console.log(preShuffleArr);
 
-  // const selectRandom = Math.floor(Math.random() * activeChars.length);
-  // console.log(selectRandom);
-  // console.log(activeChars[selectRandom]);
-
-  // shuffle(activeChars);
-
-  // const generatedPassword = activeChars.slice(0, length).join('');
-  // console.log(generatedPassword);
-
-  shuffle(preShuffleArr);
-  console.log(preShuffleArr);
-  // return generatedPassword;
+  // Final password:
+  shuffle(passwordDraft);
+  const generatedPassword = passwordDraft.join('');
+  console.log(generatedPassword);
+  return generatedPassword;
 }
 
 /*
@@ -106,8 +86,7 @@ EVENT LISTENERS:
 // getPassword(2);
 getPassword(5);
 
-// selectRandom([2, 4, 5]);
-// passwordParameters.includeNumbers.enabled = false;
+// characterGroups.includeNumbers.enabled = false;
 // console.log('again');
-// passwordParameters.includeSymbols.enabled = false;
-// getPassword(25);
+// characterGroups.includeSymbols.enabled = false;
+// getPassword(100);
